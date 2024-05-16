54ff3baf87b75d39b9d59e578c3020abff74c705
Jonathan Lilly <jmlilly@psi.edu>
2024-05-16 13:53:38 -0400

    Fixed capitalization typos in Contents

--- Contents.m
+++ Contents.m
@@ -1,7 +1,7 @@
-%   eddycenterfromfield - Find the center of a coherent eddy from 3D fields.
+%   eddyCenterFromField - Find the center of a coherent eddy from 3D fields.
 %   fieldInEddyFrame - Create a frame centered on an eddy through interpolation.
 %   eddyFrameStreamFunction - Streamfunction of an eddy in the co-moving frame.
 %   largestClosedStreamline - Largest closed streamline in the eddy frame.
 %   isParticleEnclosed - True for particles enclosed within a given contour.
-%   radialstatisticsfromgridded - Radial statistics from gridded data.
-%   radialstatisticsfromscattered - Radial statistics from scattered data.
+%   radialStatisticsFromGridded - Radial statistics from gridded data.
+%   radialStatisticsFromScattered - Radial statistics from scattered data.
